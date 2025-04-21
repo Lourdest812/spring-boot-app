400c275a0d7d97fc457444a658f485ecb5280a05
Lourdes Toledo <lourdest812@gmail.com>
2025-04-21 00:47:20 -0300

    First commit, upload of project base

---
+++ studystacks/src/main/java/com/lourdes/studystacks/studystacks/StudyStacksApplication.java
@@ -0,0 +1,13 @@
+package com.lourdes.studystacks.studystacks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyStacksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyStacksApplication.class, args);
+	}
+
+}
---
+++ studystacks/src/test/java/com/lourdes/studystacks/studystacks/StudyStacksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lourdes.studystacks.studystacks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyStacksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
